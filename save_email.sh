3e6b097b20076fda228a2bf2af0ac04a1fd31668
tayyebi <smile@tyyi.net>
2024-07-24 00:44:29 +0330

    MTA, MDA

--- save_email.sh
+++ save_email.sh
@@ -3,9 +3,14 @@
 
 # Create a directory to store emails if it doesn't exist
 mkdir -p /var/mail/
+chmod 755 /var/mail/
 
 # Generate a unique filename based on the current timestamp
 FILENAME="/var/mail/$(date +%s%N).eml"
 
 # Save the email to the file
 cat > "$FILENAME"
+
+# Set file permissions to readonly
+chown $USER:$USER "$FILENAME"
+chmod 644 "$FILENAME"
