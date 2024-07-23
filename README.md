5b5c90c863de97cd7d6ba465c5782d891778c64e
tayyebi <smile@tyyi.net>
2024-07-24 00:46:32 +0330

    IMAP.

---
+++ README.md
@@ -0,0 +1,6 @@
+## TODOs
+[ ] Maildir format
+[ ] Global footer and reply-to address
+[ ] Reverse proxy
+[ ] POP3 exponge
+[ ] Files permissions
--- save_email.sh
+++ save_email.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # save_email.sh
 
-# Create a directory to store emails if it doesn't exist
-mkdir -p /var/mail/
-chmod 755 /var/mail/
-
 # Generate a unique filename based on the current timestamp
 FILENAME="/var/mail/$(date +%s%N).eml"
 
 # Save the email to the file
 cat > "$FILENAME"
+cat "$FILENAME" >> /var/mail/root/inbox
 
 # Set file permissions to readonly
-chown $USER:$USER "$FILENAME"
+chown root:root "$FILENAME"
 chmod 644 "$FILENAME"
