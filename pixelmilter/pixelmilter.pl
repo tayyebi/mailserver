b0966ca6ed2e2438ef27b4ff40cecc6065ae673f
Tayyebi on Dell Latitude <mohammadreza.tayyebi@abrnoc.com>
2025-11-17 19:21:14 +0330

    refactor pixelmilter to use Sendmail::PMilter; update Dockerfile to install new dependencies and remove obsolete ones

--- pixelmilter/pixelmilter.pl
+++ pixelmilter/pixelmilter.pl
@@ -2,8 +2,7 @@
 use strict;
 use warnings;
 
-use Mail::Milter::Server;
-use Mail::Milter::Context qw(SMFIS_CONTINUE SMFIS_ACCEPT SMFIS_REPLACEBODY);
+use Sendmail::PMilter qw(:all);
 use MIME::Parser;
 use MIME::Entity;
 use JSON::MaybeXS;
@@ -22,206 +21,238 @@ my $DISCLOSURE_HEADER = $ENV{DISCLOSURE_HEADER} // 'X-Tracking-Notice';
 my $INJECT_DISCLOSURE = $ENV{INJECT_DISCLOSURE} // 1;
 my $LOG_PATH = $ENV{LOG_PATH} // '/data/pixel/milter.log';
 
+# Global variables to store per-connection state
+my %connections = ();
+
 ###############################################
-# Per-message milter filter object
+# Milter callback functions
 ###############################################
 
-{
-    package PixelFilter;
-    use parent 'Mail::Milter::Filter';
+sub milter_connect {
+    my ($ctx, $hostname, $sockaddr) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    $connections{$conn_id} = {
+        hostname => $hostname,
+        sockaddr => $sockaddr,
+    };
+    
+    _log("New connection from $hostname ($sockaddr), ID: $conn_id");
+    return SMFIS_CONTINUE;
+}
 
-    sub new {
-        my ($class) = @_;
-        return bless {
-            id => undef,
-            headers => {},
-            raw_headers => "",
-            body => "",
-            should_track => 0,
-            sender => "",
-        }, $class;
+sub milter_envfrom {
+    my ($ctx, $sender) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    my $msg_id = _generate_id();
+    
+    $connections{$conn_id}{message} = {
+        id => $msg_id,
+        sender => $sender,
+        headers => {},
+        raw_headers => "",
+        body => "",
+        should_track => 0,
+    };
+    
+    _log("New message from $sender, ID: $msg_id");
+    return SMFIS_CONTINUE;
+}
+
+sub milter_header {
+    my ($ctx, $name, $value) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    my $msg = $connections{$conn_id}{message} or return SMFIS_CONTINUE;
+    
+    # Store headers in both formats
+    $msg->{raw_headers} .= "$name: $value\n";
+    $msg->{headers}{lc($name)} = $value;
+    
+    # Check for opt-in header
+    if ($REQUIRE_OPT_IN && lc($name) eq lc($OPT_IN_HEADER)) {
+        $msg->{should_track} = ($value =~ /^(yes|true|1|on)$/i);
+        _log("Opt-in header found: $name: $value, tracking: $msg->{should_track}");
     }
+    
+    return SMFIS_CONTINUE;
+}
 
-    sub envfrom {
-        my ($self, $ctx, $sender) = @_;
-        $self->{id} = _generate_id();
-        $self->{sender} = $sender;
-        $self->{headers} = {};
-        $self->{raw_headers} = "";
-        $self->{body} = "";
-        $self->{should_track} = 0;
-        
-        _log("New message from $sender, ID: $self->{id}");
-        return SMFIS_CONTINUE;
+sub milter_eoh {
+    my ($ctx) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    my $msg = $connections{$conn_id}{message} or return SMFIS_CONTINUE;
+    
+    $msg->{raw_headers} .= "\n";
+    
+    # If opt-in is not required, enable tracking by default
+    if (!$REQUIRE_OPT_IN) {
+        $msg->{should_track} = 1;
     }
-
-    sub header {
-        my ($self, $ctx, $name, $value) = @_;
-        
-        # Store headers in both formats
-        $self->{raw_headers} .= "$name: $value\n";
-        $self->{headers}{lc($name)} = $value;
-        
-        # Check for opt-in header
-        if ($REQUIRE_OPT_IN && lc($name) eq lc($OPT_IN_HEADER)) {
-            $self->{should_track} = ($value =~ /^(yes|true|1|on)$/i);
-            _log("Opt-in header found: $name: $value, tracking: $self->{should_track}");
-        }
-        
-        return SMFIS_CONTINUE;
+    
+    # Add disclosure header if configured
+    if ($INJECT_DISCLOSURE && $msg->{should_track}) {
+        $ctx->chgheader($DISCLOSURE_HEADER, 1, "This email contains tracking pixels");
     }
+    
+    return SMFIS_CONTINUE;
+}
 
-    sub eoh {
-        my ($self, $ctx) = @_;
-        $self->{raw_headers} .= "\n";
-        
-        # If opt-in is not required, enable tracking by default
-        if (!$REQUIRE_OPT_IN) {
-            $self->{should_track} = 1;
-        }
-        
-        # Add disclosure header if configured
-        if ($INJECT_DISCLOSURE && $self->{should_track}) {
-            $ctx->chgheader($DISCLOSURE_HEADER, 1, "This email contains tracking pixels");
-        }
-        
-        return SMFIS_CONTINUE;
-    }
+sub milter_body {
+    my ($ctx, $chunk) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    my $msg = $connections{$conn_id}{message} or return SMFIS_CONTINUE;
+    
+    $msg->{body} .= $chunk;
+    return SMFIS_CONTINUE;
+}
 
-    sub body {
-        my ($self, $ctx, $chunk) = @_;
-        $self->{body} .= $chunk;
-        return SMFIS_CONTINUE;
-    }
-
-    sub eom {
-        my ($self, $ctx) = @_;
+sub milter_eom {
+    my ($ctx) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    my $msg = $connections{$conn_id}{message} or return SMFIS_ACCEPT;
+    
+    eval {
+        my $id = $msg->{id};
+        my $base = "/data/pixel/$id";
         
-        eval {
-            my $id = $self->{id};
-            my $base = "/data/pixel/$id";
-            
-            make_path($base);
-            
-            # Save original message data
-            write_file("$base/headers.txt", $self->{raw_headers});
-            write_file("$base/body.txt", $self->{body});
-            
-            my $meta = {
-                id => $id,
-                created => time(),
-                created_str => scalar localtime,
-                sender => $self->{sender},
-                size => length($self->{raw_headers}) + length($self->{body}),
-                tracking_enabled => $self->{should_track} ? JSON::MaybeXS::true : JSON::MaybeXS::false,
-                opened => JSON::MaybeXS::false,
-                open_count => 0,
-                first_open => undef,
-                last_open => undef,
-            };
-            
-            write_file("$base/meta.json", encode_json($meta));
-            
-            # Inject pixel if tracking is enabled
-            if ($self->{should_track}) {
-                my $modified_body = $self->_inject_pixel($self->{body}, $id);
-                if ($modified_body ne $self->{body}) {
-                    $ctx->replacebody($modified_body);
-                    _log("Pixel injected for message $id");
-                } else {
-                    _log("No HTML content found for pixel injection in message $id");
-                }
+        make_path($base);
+        
+        # Save original message data
+        write_file("$base/headers.txt", $msg->{raw_headers});
+        write_file("$base/body.txt", $msg->{body});
+        
+        my $meta = {
+            id => $id,
+            created => time(),
+            created_str => scalar localtime,
+            sender => $msg->{sender},
+            size => length($msg->{raw_headers}) + length($msg->{body}),
+            tracking_enabled => $msg->{should_track} ? JSON::MaybeXS::true : JSON::MaybeXS::false,
+            opened => JSON::MaybeXS::false,
+            open_count => 0,
+            first_open => undef,
+            last_open => undef,
+        };
+        
+        write_file("$base/meta.json", encode_json($meta));
+        
+        # Inject pixel if tracking is enabled
+        if ($msg->{should_track}) {
+            my $modified_body = _inject_pixel($msg->{body}, $id);
+            if ($modified_body ne $msg->{body}) {
+                $ctx->replacebody($modified_body);
+                _log("Pixel injected for message $id");
             } else {
-                _log("Tracking disabled for message $id");
+                _log("No HTML content found for pixel injection in message $id");
             }
-            
-        };
-        
-        if ($@) {
-            _log("Error processing message $self->{id}: $@");
+        } else {
+            _log("Tracking disabled for message $id");
         }
         
-        return SMFIS_ACCEPT;
+    };
+    
+    if ($@) {
+        _log("Error processing message $msg->{id}: $@");
     }
     
-    sub _inject_pixel {
-        my ($self, $body, $id) = @_;
-        
-        # Try to parse as MIME message
-        my $parser = MIME::Parser->new();
-        $parser->output_to_core(1);
-        $parser->tmp_to_core(1);
-        
-        my $entity;
-        eval {
-            my $io = IO::String->new($body);
-            $entity = $parser->parse($io);
-        };
-        
-        if ($@ || !$entity) {
-            # Fallback: treat as plain text and look for HTML
-            return $self->_inject_pixel_simple($body, $id);
-        }
-        
-        # Process MIME entity
-        $self->_process_mime_entity($entity, $id);
-        
-        # Return modified body
-        return $entity->stringify;
+    return SMFIS_ACCEPT;
+}
+
+sub milter_close {
+    my ($ctx) = @_;
+    
+    my $conn_id = $ctx->getsymval('{i}') || 'unknown';
+    delete $connections{$conn_id};
+    
+    return SMFIS_CONTINUE;
+}
+
+###############################################
+# Pixel injection functions
+###############################################
+
+sub _inject_pixel {
+    my ($body, $id) = @_;
+    
+    # Try to parse as MIME message
+    my $parser = MIME::Parser->new();
+    $parser->output_to_core(1);
+    $parser->tmp_to_core(1);
+    
+    my $entity;
+    eval {
+        my $io = IO::String->new($body);
+        $entity = $parser->parse($io);
+    };
+    
+    if ($@ || !$entity) {
+        # Fallback: treat as plain text and look for HTML
+        return _inject_pixel_simple($body, $id);
     }
     
-    sub _process_mime_entity {
-        my ($self, $entity, $id) = @_;
+    # Process MIME entity
+    _process_mime_entity($entity, $id);
+    
+    # Return modified body
+    return $entity->stringify;
+}
+
+sub _process_mime_entity {
+    my ($entity, $id) = @_;
+    
+    my $content_type = $entity->mime_type || '';
+    
+    if ($content_type =~ /^text\/html/i) {
+        # HTML part - inject pixel
+        my $body = $entity->bodyhandle->as_string;
+        my $modified = _inject_pixel_html($body, $id);
         
-        my $content_type = $entity->mime_type || '';
-        
-        if ($content_type =~ /^text\/html/i) {
-            # HTML part - inject pixel
-            my $body = $entity->bodyhandle->as_string;
-            my $modified = $self->_inject_pixel_html($body, $id);
-            
-            if ($modified ne $body) {
-                my $io = IO::String->new($modified);
-                $entity->bodyhandle($io);
-            }
-        } elsif ($content_type =~ /^multipart\//i) {
-            # Multipart - process each part
-            for my $part ($entity->parts) {
-                $self->_process_mime_entity($part, $id);
-            }
+        if ($modified ne $body) {
+            my $io = IO::String->new($modified);
+            $entity->bodyhandle($io);
         }
+    } elsif ($content_type =~ /^multipart\//i) {
+        # Multipart - process each part
+        for my $part ($entity->parts) {
+            _process_mime_entity($part, $id);
+        }
+    }
+}
+
+sub _inject_pixel_simple {
+    my ($body, $id) = @_;
+    
+    # Simple HTML detection and injection
+    if ($body =~ /<html\b/i || $body =~ /<body\b/i) {
+        return _inject_pixel_html($body, $id);
     }
     
-    sub _inject_pixel_simple {
-        my ($self, $body, $id) = @_;
-        
-        # Simple HTML detection and injection
-        if ($body =~ /<html\b/i || $body =~ /<body\b/i) {
-            return $self->_inject_pixel_html($body, $id);
-        }
-        
-        return $body;
+    return $body;
+}
+
+sub _inject_pixel_html {
+    my ($html, $id) = @_;
+    
+    my $pixel_url = $PIXEL_BASE_URL . $id;
+    my $pixel_img = qq{<img src="$pixel_url" width="1" height="1" style="display:none;border:0;outline:0;" alt="" />};
+    
+    # Try to inject before closing body tag
+    if ($html =~ s{(</body\s*>)}{$pixel_img$1}i) {
+        return $html;
     }
     
-    sub _inject_pixel_html {
-        my ($self, $html, $id) = @_;
-        
-        my $pixel_url = $PIXEL_BASE_URL . $id;
-        my $pixel_img = qq{<img src="$pixel_url" width="1" height="1" style="display:none;border:0;outline:0;" alt="" />};
-        
-        # Try to inject before closing body tag
-        if ($html =~ s{(</body\s*>)}{$pixel_img$1}i) {
-            return $html;
-        }
-        
-        # Try to inject before closing html tag
-        if ($html =~ s{(</html\s*>)}{$pixel_img$1}i) {
-            return $html;
-        }
-        
-        # Fallback: append to end
-        return $html . $pixel_img;
+    # Try to inject before closing html tag
+    if ($html =~ s{(</html\s*>)}{$pixel_img$1}i) {
+        return $html;
     }
+    
+    # Fallback: append to end
+    return $html . $pixel_img;
 }
 
 ###############################################
@@ -273,12 +304,6 @@ make_path($socket_dir) unless -d $socket_dir;
 _log("Starting pixel milter server on socket: $socket");
 _log("Configuration: PIXEL_BASE_URL=$PIXEL_BASE_URL, REQUIRE_OPT_IN=$REQUIRE_OPT_IN");
 
-my $server = Mail::Milter::Server->new({
-    unix => $socket,
-});
-
-$server->register('pixel' => PixelFilter->new());
-
 # Set signal handlers for graceful shutdown
 $SIG{TERM} = $SIG{INT} = sub {
     _log("Received shutdown signal, cleaning up...");
@@ -286,5 +311,18 @@ $SIG{TERM} = $SIG{INT} = sub {
     exit 0;
 };
 
+# Create and run the milter
+my $milter = Sendmail::PMilter->new();
+
+$milter->register('pixelmilter', {
+    'connect' => \&milter_connect,
+    'envfrom' => \&milter_envfrom,
+    'header'  => \&milter_header,
+    'eoh'     => \&milter_eoh,
+    'body'    => \&milter_body,
+    'eom'     => \&milter_eom,
+    'close'   => \&milter_close,
+}, SMFI_CURR_ACTS);
+
 _log("Pixel milter server started successfully");
-$server->main();
+$milter->main($socket);
